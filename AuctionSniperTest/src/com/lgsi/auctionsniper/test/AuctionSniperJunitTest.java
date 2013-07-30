6c13abd0397677e46b7d58b59f726f843fa808b8
TapanGithub <tapan.mouli@gmail.com>
2013-07-30 19:47:57 +0900

    Sample Test case Structure Using Robotium Added

--- AuctionSniperTest/src/com/lgsi/auctionsniper/test/AuctionSniperJunitTest.java
+++ AuctionSniperTest/src/com/lgsi/auctionsniper/test/AuctionSniperJunitTest.java
@@ -1,5 +1,6 @@
 package com.lgsi.auctionsniper.test;
 //import com.jayway.android.robotium.solo.Solo;
+//import com.jayway.android.robotium.solo.Solo;
 import com.lgsi.auctionsniper.MainActivity;
 
 import android.test.ActivityInstrumentationTestCase2;
@@ -11,13 +12,20 @@ public class AuctionSniperJunitTest extends ActivityInstrumentationTestCase2<Mai
 	public AuctionSniperJunitTest(String name) {
 		super(MainActivity.class);
 	}
-
+	
+	//private Solo solo;
+	@Override
 	protected void setUp() throws Exception {
-		super.setUp();
+	//solo = new Solo(getInstrumentation(),getActivity());
 	}
-
-	protected void tearDown() throws Exception {
-		super.tearDown();
+	
+	// Need to add the test Cases.
+	
+	
+	@Override
+	public void tearDown() throws Exception {
+	//solo.finishOpenedActivities();
 	}
-
+	
+	
 }
